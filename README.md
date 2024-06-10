0715668968fc89c1dc5898e49699d38ff05cb1b9
María José <majocosta4@gmail.com>
2024-06-09 22:09:56 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# analizador_lexico_sintactico
+ Analizador web Lexico, sintactio para el lenguaje de Java
---
+++ app.py
@@ -0,0 +1,20 @@
+# app.py
+
+from flask import Flask, request, render_template
+from lexer import tokenize
+from parser_1 import parse
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    result_lex = []
+    result_syntax = ''
+    if request.method == 'POST':
+        code = request.form['code']
+        result_lex = tokenize(code)
+        result_syntax = parse(code)
+    return render_template('index.html', tokens=result_lex, syntax=result_syntax)
+
+if __name__ == '__main__':
+    app.run(debug=True)
